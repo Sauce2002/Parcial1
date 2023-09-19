c2090bac308e6dc9ed83f99ac6bc92245f22f0e7
Sauce2002 <jjpro2002@gmail.com>
2023-09-19 18:17:23 -0500

    .

--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@ int main()
     return 0;
 }
 
-void imagen(){
+void imagen(){//matriz del usuario
     int matrizUsuario[8][8]={};
     //int *pMatrizPuntero = &matrizUsuario[8][8];
     cout<<"ingrese un 1 para encendido y 0 para apagado"<<endl;
     for(int i =0;i<8;i++){
-        for(int j=0;j<8;i++){
+        for(int j=0;j<8;j++){
             int a=0;
-            cout << "ingrese estado del led en la posicion ["<<j+1<<"]["<<i+1<<"]: "<<endl;
+            cout << "ingrese estado del led en la posici0on ["<<i+1<<"]["<<j+1<<"]: "<<endl;
             cin >>a;
             matrizUsuario[i][j]=a;
         }
