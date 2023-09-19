5b0bd9cdbc948bfdd58e45d04a22f11633d2cf81
Sauce2002 <jjpro2002@gmail.com>
2023-09-19 18:03:11 -0500

    .

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 
 using namespace std;
-
+void imagen();
 int main()
 {
     cout << "Hello World!" << endl;
+    imagen();
     return 0;
 }
 
 void imagen(){
     int matrizUsuario[8][8]={};
-
+    //int *pMatrizPuntero = &matrizUsuario[8][8];
+    cout<<"ingrese un 1 para encendido y 0 para apagado"<<endl;
+    for(int i =0;i<8;i++){
+        for(int j=0;j<8;i++){
+            int a=0;
+            cout << "ingrese estado del led en la posicion ["<<j+1<<"]["<<i+1<<"]: "<<endl;
+            cin >>a;
+            matrizUsuario[i][j]=a;
+        }
+    }
+    for(int x=0;x<8;x++){
+        for(int z=0; z<8;z++){
+            cout<< matrizUsuario[x][z];
+        }
+        cout<<endl;
+    }
 }
