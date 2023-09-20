22bf5f17c86ba49664c4973aa2237093204b50ac
Sauce2002 <jjpro2002@gmail.com>
2023-09-19 19:11:40 -0500

    funcion imagen

--- main.cpp
+++ main.cpp
@@ -11,14 +11,16 @@ int main()
 
 void imagen(){//matriz del usuario
     int matrizUsuario[8][8]={};
-    //int *pMatrizPuntero = &matrizUsuario[8][8];
+    int (*pMatrizPuntero)[8][8]=&matrizUsuario;
+
     cout<<"ingrese un 1 para encendido y 0 para apagado"<<endl;
-    for(int i =0;i<8;i++){
+    for(int i=0;i<8;i++){
         for(int j=0;j<8;j++){
             int a=0;
             cout << "ingrese estado del led en la posici0on ["<<i+1<<"]["<<j+1<<"]: "<<endl;
             cin >>a;
-            matrizUsuario[i][j]=a;
+            //&(*(pMatrizPuntero+j))+i=a;
+            //matrizUsuario[i][j]=a;
         }
     }
     for(int x=0;x<8;x++){
